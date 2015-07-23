1149313b612693ce33c00d9014f64a2e162327f7
Sibo <oyu-gio@hotmail.com>
2015-07-22 21:11:54 -0500

    Initial Commit
    Initial Commit

--- Navigation Bars/ViewController.swift
+++ Navigation Bars/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var timerOutput: UILabel!
+    
+    @IBOutlet var lapOutput: UILabel!
+    
+    @IBAction func timerStart(sender: AnyObject) {
+        if(!timer.valid) {
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true);
+        }
+    }
+    
+    @IBAction func timerStop(sender: AnyObject) {
+        if(timer.valid) {
+            timer.invalidate();
+            isPaused = true;
+        }
+    }
+    
+    @IBAction func timerReset(sender: AnyObject) {
+        count = 0;
+        timerOutput.text = "";
+        lapOutput.text = "";
+    }
+    
+    @IBAction func timerLaps(sender: AnyObject) {
+        lapOutput.text = lapOutput.text! + "\n" + count.description;
+    }
+    
+    var timer = NSTimer();
+    
+    var count = 0;
+    
+    var isPaused = false;
+    
+    func result() {
+        count++;
+        timerOutput.text = count.description;
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
